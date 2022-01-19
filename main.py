e41c676518a3d293d62b957d07b199d534ecb18a
JorsinLuvhart <80300960+JorsinLuvhart@users.noreply.github.com>
2022-01-19 10:17:57 +0000

    Grid and objects with coordinates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import arcade
-
+import random
 print("Python version " + sys.version)
 
 
@@ -13,12 +13,31 @@ MARGIN = 2
 COLUMN_COUNT = int(SCREEN_WIDTH/(BOX_LENGTH+MARGIN))
 ROW_COUNT = int(SCREEN_WIDTH/(BOX_LENGTH+MARGIN))
 SCREEN_TITLE = "Cooperative Bots Design"
-VIEWPORT_MARGIN = 200
-MOVEMENT_SPEED = 5
+MOVEMENT_SPEED = 1
+NUM_BOTS = 1
+NUM_DESTI = 1
+NUM_PARCEL = 1
 
 #warehouse floor, 0=blank space, 1=robot, 2=parcel, 3=destination
+class Robot():
+    def __init__(self, sprite):
+      self.x = 0
+      self.y = 0
+      self.sprite = sprite
 
-class gameWindow(arcade.Window):
+class Parcel():
+    def __init__(self, sprite):
+      self.x = random.randint(1, ROW_COUNT)
+      self.y = random.randint(1, COLUMN_COUNT)
+      self.sprite = sprite
+  
+class Destination():
+    def __init__(self, sprite):
+      self.x = random.randint(1, ROW_COUNT)
+      self.y = random.randint(1, COLUMN_COUNT)
+      self.sprite = sprite
+
+class GameWindow(arcade.Window):
   
   def __init__(self):
     """ Initialise object here"""
@@ -29,7 +48,6 @@ class gameWindow(arcade.Window):
     self.parcelList = None
     self.destinationList = None
     self.gridSpriteList = None
-
 
   def setup(self):
     """ Set up the game here. Call this function to restart the game. """
@@ -51,16 +69,51 @@ class gameWindow(arcade.Window):
         self.gridSpriteList.append(sprite)
         self.gridSprites[row].append(sprite)
 
-  
+    #robot sprite
+    sprite = arcade.Sprite("Resources/robot.png")
+    self.robot = Robot(sprite)
+    self.robot.sprite.center_x = self.robot.x * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.robot.sprite.center_y = self.robot.y * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.robotList.append(self.robot.sprite)
+
+    #destination sprite
+    sprite = arcade.Sprite("Resources/destination.png")
+    self.desti = Destination(sprite)
+    self.desti.sprite.center_x = self.desti.x * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.desti.sprite.center_y = self.desti.y * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.destinationList.append(self.desti.sprite)
+
+    sprite = arcade.Sprite("Resources/parcel.png")
+    self.parcel = Parcel(sprite)
+    self.parcel.sprite.center_x = self.parcel.x * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.parcel.sprite.center_y = self.parcel.y * (BOX_LENGTH + MARGIN) + (BOX_LENGTH + MARGIN)/2
+    self.parcelList.append(self.parcel.sprite)
+
   def on_draw(self):
     """Render the screen."""
     arcade.start_render()
+    
     # Code to draw the screen goes here
     self.gridSpriteList.draw()
+    self.robotList.draw()
+    self.parcelList.draw()
+    self.destinationList.draw()
+
+
+  def on_key_press(self, key, modifiers):
+    """Called whenever a key is pressed. """
+    if key == arcade.key.UP:
+        self.player_sprite.change_y = MOVEMENT_SPEED
+    elif key == arcade.key.DOWN:
+        self.player_sprite.change_y = -MOVEMENT_SPEED
+    elif key == arcade.key.LEFT:
+        self.player_sprite.change_x = -MOVEMENT_SPEED
+    elif key == arcade.key.RIGHT:
+        self.player_sprite.change_x = MOVEMENT_SPEED
 
 def main():
     """Main function"""
-    window = gameWindow()
+    window = GameWindow()
     window.setup()
     arcade.run()
 
